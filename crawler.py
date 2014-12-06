51f3a2d4188724a8c35f00878a335730f1014d83
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-12-06 18:46:42 -0500

    updating crawler

--- crawler.py
+++ crawler.py
@@ -80,7 +80,7 @@ for aURI in total_articles:
     qAr.addRequestedResult(RequestArticleInfo())
     qAr.addRequestedResult(RequestArticleDuplicatedArticles(count=1000))
     articleRes = er.execQuery(qAr)
-    if articleRes:
+    try:
         # Store article information
         articleInfo[aURI] = {}
         articleInfo[aURI]['title'] = articleRes['info']['title']
@@ -96,6 +96,8 @@ for aURI in total_articles:
         for d in articleRes['duplicatedArticles']['results']:
             articleDuplicates[aURI].append(d['uri'])
         # Update progress bar
+    except:
+        pass
     articles_checked += 1.0
     progress = articles_checked*100.0/float(total_entries)
     sys.stdout.write("Article infromation extraction progress: %10.2f%% (%d out of %d)   \r" % (progress,articles_checked,total_entries))
