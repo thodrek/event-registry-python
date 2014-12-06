add314874bed90d35d36f59a65adf2590b4b7783
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-12-06 18:47:42 -0500

    updating crawler

--- crawler.py
+++ crawler.py
@@ -53,9 +53,12 @@ for eURI in eventURIs['uriList']:
     qEv = QueryEvent(eURI)
     qEv.addRequestedResult(RequestEventArticleUris(lang=["eng"]))
     eventRes = er.execQuery(qEv)
-    # Store result
-    eventToArticleMap[eURI] = eventRes['articleUris']
-    total_articles.extend(eventRes['articleUris'])
+    try:
+        # St ore result
+        eventToArticleMap[eURI] = eventRes['articleUris']
+        total_articles.extend(eventRes['articleUris'])
+    except:
+        pass
     # Update progress bar
     events_checked += 1.0
     progress = events_checked*100.0/float(total_entries)
